a6123d87ebc5d9c10c71b05fcc37962e6e544333
Ryan Ransford <rdransfo@gmail.com>
2011-06-12 13:53:14 -0500

    refactoring new methods in RestResourceTestHelper

--- chit-chat/src/test/java/com/blogspot/activeactive/chitchat/resources/RestResourceTestHelper.java
+++ chit-chat/src/test/java/com/blogspot/activeactive/chitchat/resources/RestResourceTestHelper.java
@@ -6,6 +6,7 @@ import static javax.ws.rs.core.MediaType.APPLICATION_XML;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
@@ -16,7 +17,6 @@ import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -24,6 +24,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -33,32 +34,33 @@ public class RestResourceTestHelper {
 
 	private HttpClient client;
 
-	public HttpClient getClient() {
-		if (client == null) {
-			client = new DefaultHttpClient();
-		}
-		return client;
-	}
-
 	public String getBaseUrl() {
 		return "http://localhost:8080/resources";
 	}
 
-	public InputStream getContentStream(final String path,
-			final String mediaType) throws IOException, ClientProtocolException {
-		final HttpResponse rsp = getResponse(path, mediaType);
-		return rsp.getEntity().getContent();
+	public HttpClient getClient() {
+		if (client == null) {
+			client = new DefaultHttpClient(new ThreadSafeClientConnManager());
+		}
+		return client;
+	}
+
+	public InputStream getContentStream(final HttpResponse response)
+			throws IllegalStateException, IOException {
+		return response.getEntity().getContent();
 	}
 
 	public JSONArray getPayloadArrayFromJson(final String path)
 			throws ClientProtocolException, IOException, JSONException {
-		final InputStream content = getContentStream(path, APPLICATION_JSON);
+		final HttpResponse rsp = getResponse(path, APPLICATION_JSON);
+		final InputStream content = getContentStream(rsp);
 		return new JSONArray(new JSONTokener(new InputStreamReader(content)));
 	}
 
 	public JSONObject getPayloadFromJson(final String path)
 			throws ClientProtocolException, IOException, JSONException {
-		final InputStream content = getContentStream(path, APPLICATION_JSON);
+		final HttpResponse rsp = getResponse(path, APPLICATION_JSON);
+		final InputStream content = getContentStream(rsp);
 		return new JSONObject(new JSONTokener(new InputStreamReader(content)));
 	}
 
@@ -66,48 +68,53 @@ public class RestResourceTestHelper {
 			final Class<T> expectedClass) throws JAXBException, IOException,
 			ClientProtocolException, FactoryConfigurationError,
 			XMLStreamException {
-		final XMLInputFactory f = XMLInputFactory.newFactory();
-		final XMLStreamReader reader = f
-				.createXMLStreamReader(getContentStream(path, APPLICATION_XML));
-		final JAXBContext ctx = JAXBContext.newInstance(expectedClass);
-		final Unmarshaller um = ctx.createUnmarshaller();
-		final JAXBElement<T> el = um.unmarshal(reader, expectedClass);
-		return el.getValue();
+		final HttpResponse rsp = getResponse(path, APPLICATION_XML);
+		final InputStream content = getContentStream(rsp);
+		return unMarshall(content, expectedClass);
+	}
+
+	public HttpResponse getResponse(final String path, final String mediaType)
+			throws IOException, ClientProtocolException {
+		final HttpGet get = new HttpGet(getBaseUrl() + path);
+		get.addHeader("Accept", mediaType);
+		return getClient().execute(get);
+	}
+
+	public JSONObject postJsonPayloadAndGetJsonPayload(final String path,
+			final String payload) throws ClientProtocolException, IOException,
+			JSONException {
+		final HttpResponse rsp = postResponse(path, payload, APPLICATION_JSON);
+		final InputStream content = getContentStream(rsp);
+		return new JSONObject(new JSONTokener(new InputStreamReader(content)));
+	}
+
+	public HttpResponse postResponse(final String path, final String payload,
+			String mediaType) throws UnsupportedEncodingException, IOException,
+			ClientProtocolException {
+		final HttpPost post = new HttpPost(getBaseUrl() + path);
+		post.addHeader("Accept", mediaType);
+		post.setEntity(new StringEntity(payload, mediaType, "UTF-8"));
+		return getClient().execute(post);
 	}
 
 	public <T> T postXmlPayloadAndGetXmlPayload(final String path,
 			final String payload, final Class<T> expectedClass)
 			throws ClientProtocolException, IOException, XMLStreamException,
 			JAXBException {
-		final HttpPost post = new HttpPost(getBaseUrl() + path);
-		post.addHeader("Accept", APPLICATION_XML);
-		post.setEntity(new StringEntity(payload, APPLICATION_XML, "UTF-8"));
-		final HttpResponse rsp = getClient().execute(post);
-		final InputStream is = rsp.getEntity().getContent();
+		final HttpResponse rsp = postResponse(path, payload, APPLICATION_XML);
+		final InputStream content = getContentStream(rsp);
+		return unMarshall(content, expectedClass);
+	}
+
+	public <T> T unMarshall(final InputStream contentStream,
+			final Class<T> expectedClass)
+			throws javax.xml.stream.FactoryConfigurationError,
+			XMLStreamException, JAXBException {
 		final XMLInputFactory f = XMLInputFactory.newFactory();
-		final XMLStreamReader reader = f.createXMLStreamReader(is);
+		final XMLStreamReader reader = f.createXMLStreamReader(contentStream);
 		final JAXBContext ctx = JAXBContext.newInstance(expectedClass);
 		final Unmarshaller um = ctx.createUnmarshaller();
 		final JAXBElement<T> el = um.unmarshal(reader, expectedClass);
 		return el.getValue();
-	}
-
-	public JSONObject postJsonPayloadAndGetJsonPayload(final String path,
-			final String payload) throws ClientProtocolException, IOException,
-			JSONException {
-		final HttpPost post = new HttpPost(getBaseUrl() + path);
-		post.addHeader("Accept", APPLICATION_JSON);
-		post.setEntity(new StringEntity(payload, APPLICATION_JSON, "UTF-8"));
-		final HttpResponse rsp = getClient().execute(post);
-		final InputStream is = rsp.getEntity().getContent();
-		final String content = IOUtils.toString(is);
-		return new JSONObject(new JSONTokener(content));
-	}
-
-	public HttpResponse getResponse(final String path,
-			final String mediaType) throws IOException, ClientProtocolException {
-		final HttpGet get = new HttpGet(getBaseUrl() + path);
-		get.addHeader("Accept", mediaType);
-		return getClient().execute(get);
 	}
 }
